fc9a881bdc66153f30457fdea06be8288a86de13
Kiran.Shivalingaiah <Kiran.Shivalingaiah@target.com>
2020-04-20 15:32:36 +0530

    Remove item_title from PUT/{list_id}/list_items/{list_item_id}

--- favorites-service/src/main/kotlin/com/tgt/favorites/transport/FavoriteListItemUpdateRequestTO.kt
+++ favorites-service/src/main/kotlin/com/tgt/favorites/transport/FavoriteListItemUpdateRequestTO.kt
@@ -3,10 +3,9 @@ package com.tgt.favorites.transport
 import com.tgt.lists.lib.api.transport.ListItemUpdateRequestTO
 
 class FavoriteListItemUpdateRequestTO(
-    val itemTitle: String,
     val itemNote: String
 ) {
     fun toListItemUpdateRequestTO(): ListItemUpdateRequestTO {
-        return ListItemUpdateRequestTO(itemTitle = itemTitle, itemNote = itemNote)
+        return ListItemUpdateRequestTO(itemNote = itemNote)
     }
 }
